d79248e7cdb5aa7f64945c87739661ce2cd379cf
Aakash Dhal <45394245+KRITIKALai@users.noreply.github.com>
2021-12-15 16:58:02 +0530

    Create multiplicationUsingRecursion.cpp

---
+++ multiplicationUsingRecursion.cpp
@@ -0,0 +1,14 @@
+// Recursive function to multiply 2 numbers a*b
+//without using * operator
+#include <iostream>
+using namespace std;
+int mul(int a, int b){
+    if(b==0) return 0;
+    return a+mul(a,b-1);
+}
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<mul(a,b);
+    return 0;
+}
